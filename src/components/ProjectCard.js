f67e4e3a11617dafd49b76b2ffb4c178d07c995f
Renuka Choudhary <renurunrun@gmail.com>
2023-05-17 23:46:48 +0530

    improved

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -9,7 +9,7 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import './ProjectCard.css';
-import { Link } from 'react-router-dom';
+
 
 // <CardMedia
 // component="img"
